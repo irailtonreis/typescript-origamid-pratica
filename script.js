8f63d5a5e223dc1cc8ad4fe0ac8aea46824799e9
irailtonreis <irailton.oliveira91@gmail.com>
2023-06-26 17:17:13 -0300

    feat: Add annotation prática

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ function somar(a, b) {
     return a + b;
 }
 console.log(somar(10, 5));
+function normalizarTexto(texto) {
+    return texto.trim().toLowerCase();
+}
+normalizarTexto(" DesigNer");
--- script.ts
+++ script.ts
@@ -5,3 +5,10 @@ function somar(a: number, b: number) {
 }   
 
 console.log(somar(10, 5))
+
+
+function normalizarTexto(texto: string) {
+    return texto.trim().toLowerCase()
+}
+
+normalizarTexto(" DesigNer")
