0a84cb53a2737847f75f4c4faa1168db60a263f6
irailtonreis <irailton.oliveira91@gmail.com>
2023-06-27 12:49:08 -0300

    feat: typeof funtion

--- script.js
+++ script.js
@@ -7,35 +7,59 @@
 //     return texto.trim().toLowerCase()
 // }
 // normalizarTexto(" DesigNer")
-var input = document.querySelector("input");
-var total = localStorage.getItem("total");
-if (input && total) {
-    input.value = total;
-    calcularGanho(Number(input.value));
-}
-function calcularGanho(value) {
-    var p = document.querySelector("p");
-    if (p) {
-        p.innerText = "ganho total: ".concat(value + 100 - value * 0.2);
+// const input = document.querySelector("input");
+// const total = localStorage.getItem("total");
+// if (input && total) {
+//     input.value = total;
+//     calcularGanho(Number(input.value));
+// }
+// function calcularGanho(value: number) {
+//     const p = document.querySelector("p");
+//     if (p) {
+//         p.innerText = `ganho total: ${value + 100 - value * 0.2}`;
+//     }
+// }
+// function totalMudou() {
+//     if (input) {
+//         localStorage.setItem("total", input.value);
+//         calcularGanho(Number(input.value));
+//     }
+// }
+// if (input) input.addEventListener("keyup", totalMudou)
+// const frase = "Front End";
+// const preco = 500;
+// const condi = preco > 100;
+// console.log(typeof frase);
+// console.log(typeof preco);
+// console.log(typeof null);
+// if (typeof frase === "string") {
+//     frase.toLowerCase();
+// } else {
+//     console.log("Frase ndo é uma string");
+// }
+// let total: string | number = 200;
+// total = "4000";
+// function isNumber(value: string | number) {
+//     if (typeof value === "number") {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+// if (isNumber("200")) {
+//     console.log("E um nimero");
+// }
+// const button = document.querySelector("button")
+// button?.click()
+function toNumber(param) {
+    if (typeof param === "number") {
+        return param;
     }
-}
-function totalMudou() {
-    if (input) {
-        localStorage.setItem("total", input.value);
-        calcularGanho(Number(input.value));
+    if (typeof param === "string") {
+        return Number(param);
     }
+    throw new Error("O parâmetro de ser um número ou string");
 }
-if (input)
-    input.addEventListener("keyup", totalMudou);
-var frase = "Front End";
-var preco = 500;
-var condi = preco > 100;
-console.log(typeof frase);
-console.log(typeof preco);
-console.log(typeof null);
-if (typeof frase === "string") {
-    frase.toLowerCase();
-}
-else {
-    console.log("Frase ndo é uma string");
-}
+console.log(toNumber("9"));
+console.log(toNumber(10));
+console.log(toNumber(true));
--- script.ts
+++ script.ts
@@ -14,39 +14,73 @@
 // normalizarTexto(" DesigNer")
 
 
-const input = document.querySelector("input");
-const total = localStorage.getItem("total");
-if (input && total) {
-    input.value = total;
-    calcularGanho(Number(input.value));
-}
+// const input = document.querySelector("input");
+// const total = localStorage.getItem("total");
+// if (input && total) {
+//     input.value = total;
+//     calcularGanho(Number(input.value));
+// }
 
 
-function calcularGanho(value: number) {
-    const p = document.querySelector("p");
-    if (p) {
-        p.innerText = `ganho total: ${value + 100 - value * 0.2}`;
-    }
-}
+// function calcularGanho(value: number) {
+//     const p = document.querySelector("p");
+//     if (p) {
+//         p.innerText = `ganho total: ${value + 100 - value * 0.2}`;
+//     }
+// }
 
-function totalMudou() {
-    if (input) {
-        localStorage.setItem("total", input.value);
-        calcularGanho(Number(input.value));
-    }
-}
-if (input) input.addEventListener("keyup", totalMudou)
+// function totalMudou() {
+//     if (input) {
+//         localStorage.setItem("total", input.value);
+//         calcularGanho(Number(input.value));
+//     }
+// }
+// if (input) input.addEventListener("keyup", totalMudou)
 
 
-const frase = "Front End";
-const preco = 500;
-const condi = preco > 100;
-console.log(typeof frase);
-console.log(typeof preco);
-console.log(typeof null);
+// const frase = "Front End";
+// const preco = 500;
+// const condi = preco > 100;
+// console.log(typeof frase);
+// console.log(typeof preco);
+// console.log(typeof null);
 
-if (typeof frase === "string") {
-    frase.toLowerCase();
-} else {
-    console.log("Frase ndo é uma string");
-}
+// if (typeof frase === "string") {
+//     frase.toLowerCase();
+// } else {
+//     console.log("Frase ndo é uma string");
+// }
+
+
+// let total: string | number = 200;
+// total = "4000";
+
+// function isNumber(value: string | number) {
+//     if (typeof value === "number") {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+// if (isNumber("200")) {
+//     console.log("E um nimero");
+// }
+
+// const button = document.querySelector("button")
+
+// button?.click()
+
+// function toNumber(param: number | string){
+//     if(typeof param === "number"){
+//         return param
+//     }
+//     if(typeof param === "string"){
+//         return Number(param)
+//     }
+//     throw new Error("O parâmetro de ser um número ou string");
+    
+// }
+
+// console.log(toNumber("9"))
+// console.log(toNumber(10))
+// console.log(toNumber(true))
