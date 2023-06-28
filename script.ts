8fa6be8b3b4f949c35e4faf3220c0a1eeb90581d
irailtonreis <irailton.oliveira91@gmail.com>
2023-06-28 15:43:51 -0300

    feat: Consume retorno arrays api

--- script.ts
+++ script.ts
@@ -156,3 +156,44 @@ function showProduct(data: Produto) {
       </div>
     `;
 }
+
+// se curso inciante Azul / vermelho
+async function fetchCursos() {
+  const response = await fetch('https://api.origamid.dev/json/cursos.json');
+  const data = await response.json();
+  console.log(data)
+  mostrarCursos(data);
+}
+
+fetchCursos();
+
+interface Cursos {
+  aulas: number;
+  gratuito: boolean;
+  horas: number;
+  idAulas: number [];
+  nivel: 'iniciante' | 'avancado';
+  nome: string; 
+  tags: string []
+}
+
+function mostrarCursos(cursos: Cursos[]) {
+  cursos.forEach((curso) =>{
+    let color;
+    if(curso.nivel === "iniciante"){
+      color = 'blue';
+    }else if(curso.nivel === "avancado"){
+      color = 'red';
+    }
+
+    document.body. innerHTML += `
+      <div>
+      <h2 style="color: ${color};">${curso.nome}</h2>
+      <p>Horas: ${curso.horas}</p>
+      <p>Aulas: ${curso.aulas}</p>
+      <p>Tipo: ${curso.gratuito ? "Gratuito" : "Pago"}</p>
+      <p>Tags: ${curso.tags.join(", ")}</>
+      <p>Aulas: ${curso.idAulas.join(" | ")}</>
+      </div>`;
+  })
+}
