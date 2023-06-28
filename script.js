374065b90ed620df54dca17b01560f83c3872813
irailtonreis <irailton.oliveira91@gmail.com>
2023-06-28 16:28:11 -0300

    feat: Checagem instanceof em classes

--- script.js
+++ script.js
@@ -143,3 +143,41 @@ const jogo = {
 };
 jogo.nome?.toLowerCase();
 livro.nome?.toLowerCase();
+// Checagem com instanceof 
+class Produto {
+    nome;
+    constructor(nome) {
+        this.nome = nome;
+    }
+}
+class Livro extends Produto {
+    autor;
+    constructor(nome, autor) {
+        super(nome);
+        this.autor = autor;
+    }
+}
+class Jogo extends Produto {
+    jogadores;
+    constructor(nome, jogadores) {
+        super(nome);
+        this.jogadores = jogadores;
+    }
+}
+function buscarProduto(busca) {
+    if (busca === 'O Hobbit') {
+        return new Livro('O Hobbit', 'J. R. R. Tolkien');
+    }
+    if (busca === 'Dark Souls') {
+        return new Jogo('Dark Souls', 1);
+    }
+    return null;
+}
+const produto1 = buscarProduto('O Hobbit');
+const produto2 = buscarProduto('Dark Souls');
+if (produto1 instanceof Produto) {
+    produto1.nome;
+}
+if (produto2 instanceof Produto) {
+    produto2.nome;
+}
--- script.ts
+++ script.ts
@@ -209,3 +209,50 @@ const jogo: Product = {
 
 jogo.nome?.toLowerCase();
 livro.nome?.toLowerCase();
+
+
+// Checagem com instanceof 
+
+class Produto {
+  nome: string;
+  constructor(nome: string) {
+    this.nome = nome;
+  }
+}
+
+class Livro extends Produto {
+  autor: string;
+  constructor(nome: string, autor: string) {
+    super(nome);
+    this.autor = autor;
+  }
+}
+
+class Jogo extends Produto {
+  jogadores: number;
+  constructor(nome: string, jogadores: number) {
+    super(nome);
+    this.jogadores = jogadores;
+  }
+}
+
+function buscarProduto(busca: string) {
+  if (busca === 'O Hobbit') {
+    return new Livro('O Hobbit', 'J. R. R. Tolkien');
+  }
+  if (busca === 'Dark Souls') {
+    return new Jogo('Dark Souls', 1);
+  }
+  return null;
+}
+
+const produto1 = buscarProduto('O Hobbit');
+const produto2 = buscarProduto('Dark Souls');
+
+if (produto1 instanceof Produto) {
+  produto1.nome;
+}
+if (produto2 instanceof Produto) {
+  produto2.nome;
+}
+
