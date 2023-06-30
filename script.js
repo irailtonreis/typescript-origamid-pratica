53bd5fde7d61aff06dd4d726b82d633a9c0f3d03
irailtonreis <irailton.oliveira91@gmail.com>
2023-06-30 19:13:26 -0300

    feat: Prática overload

--- script.js
+++ script.js
@@ -238,3 +238,19 @@ function toggleMenu(event) {
     }
 }
 btnMobile?.addEventListener('pointerdown', toggleMenu);
+function normalizar(valor) {
+    if (typeof valor === "string") {
+        return valor.trim().toLowerCase();
+    }
+    else {
+        return valor.map((item) => item.trim().toLowerCase());
+    }
+}
+normalizar(" Produto ");
+normalizar(["Banana ", " UVA"]);
+function $(seletor) {
+    return document.querySelector(seletor);
+}
+$("a")?.href;
+$("video")?.volume;
+$(".teste")?.innerHTML;
--- script.ts
+++ script.ts
@@ -295,3 +295,30 @@ function toggleMenu(event: Event) {
 }
 
 btnMobile?.addEventListener('pointerdown', toggleMenu)
+
+function normalizar(valor: string): string;
+function normalizar(valor: string[]): string[];
+function normalizar(valor: string | string[]): string | string[] {
+  if (typeof valor === "string") {
+    return valor.trim().toLowerCase();
+  } else {
+    return valor.map((item) => item.trim().toLowerCase());
+  }
+}
+
+normalizar(" Produto ");
+normalizar(["Banana ", " UVA"]);
+
+// Exemplo 2
+function $(seletor: "video"): HTMLVideoElement | null;
+function $(seletor: "div"): HTMLDivElement | null;
+function $(seletor: "a"): HTMLAnchorElement | null;
+function $(seletor: string): Element | null;
+function $(seletor: string): Element | null {
+  return document.querySelector(seletor);
+}
+
+$("a")?.href;
+$("video")?.volume;
+$(".teste")?.innerHTML;
+
