0ca0cb03c820cad465012212362d6efa8f2f702f
irailtonreis <irailton.oliveira91@gmail.com>
2023-07-02 11:00:26 -0300

    feat: Prática desestruração

--- script.js
+++ script.js
@@ -303,3 +303,19 @@ video1.volume;
 video2.volume;
 video3?.volume;
 video4.volume;
+function handleClick({ currentTarget, pageX, }) {
+    if (currentTarget instanceof HTMLElement) {
+        currentTarget.innerHTML = `<h1>Mouse Click em x:${pageX}</h1>`;
+    }
+}
+document.documentElement.addEventListener('click', handleClick);
+function comparar(tipo, ...numeros) {
+    if (tipo === 'maior') {
+        return Math.max(...numeros);
+    }
+    if (tipo === 'menor') {
+        return Math.min(...numeros);
+    }
+}
+console.log(comparar('maior', 3, 2, 4, 30, 5, 6, 20));
+console.log(comparar('menor', 3, 2, 4, 1, 5, 6, 20));
--- script.ts
+++ script.ts
@@ -389,3 +389,31 @@ video1.volume;
 video2.volume;
 video3?.volume;
 (video4 as HTMLVideoElement).volume;
+
+
+function handleClick({
+  currentTarget,
+  pageX,
+}: {
+  currentTarget: EventTarget | null;
+  pageX: number;
+}) {
+  if (currentTarget instanceof HTMLElement) {
+    currentTarget.innerHTML = `<h1>Mouse Click em x:${pageX}</h1>`;
+  }
+}
+
+document.documentElement.addEventListener('click', handleClick);
+
+
+function comparar(tipo: 'maior' | 'menor', ...numeros: number[]) {
+  if (tipo === 'maior') {
+    return Math.max(...numeros);
+  }
+  if (tipo === 'menor') {
+    return Math.min(...numeros);
+  }
+}
+
+console.log(comparar('maior', 3, 2, 4, 30, 5, 6, 20));
+console.log(comparar('menor', 3, 2, 4, 1, 5, 6, 20));
