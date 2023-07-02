fdbbe81e1b3e8049f0669549aeefdc84e630ebc7
irailtonreis <irailton.oliveira91@gmail.com>
2023-07-02 10:40:06 -0300

    feat: Prática type assertion

--- script.js
+++ script.js
@@ -278,3 +278,28 @@ function typeGuard(value) {
 typeGuard('Origamid');
 typeGuard(200);
 typeGuard(document.body);
+async function fetchProduto() {
+    const response = await fetch('https://api.origamid.dev/json/notebook.json');
+    return response.json();
+}
+// Podemos usar o as em diferentes locais.
+async function handleProduto1() {
+    const produto = await fetchProduto();
+    produto.nome;
+}
+async function handleProduto2() {
+    const produto = (await fetchProduto());
+    produto.nome;
+}
+async function handleProduto3() {
+    const produto = await fetchProduto();
+    produto.nome;
+}
+const video1 = document.querySelector('.player');
+const video2 = document.querySelector('.player');
+const video3 = document.querySelector('.player');
+const video4 = document.querySelector('.player');
+video1.volume;
+video2.volume;
+video3?.volume;
+video4.volume;
--- script.ts
+++ script.ts
@@ -353,3 +353,39 @@ typeGuard('Origamid');
 typeGuard(200);
 typeGuard(document.body);
 
+interface Produto {
+  nome: string;
+  preco: number;
+}
+
+async function fetchProduto() {
+  const response = await fetch('https://api.origamid.dev/json/notebook.json');
+  return response.json() as Promise<Produto>;
+}
+
+// Podemos usar o as em diferentes locais.
+async function handleProduto1() {
+  const produto = await fetchProduto();
+  produto.nome;
+}
+
+async function handleProduto2() {
+  const produto = (await fetchProduto()) as Produto;
+  produto.nome;
+}
+
+async function handleProduto3() {
+  const produto = await fetchProduto();
+  (produto as Produto).nome;
+}
+
+
+const video1 = document.querySelector('.player') as HTMLVideoElement;
+const video2 = <HTMLVideoElement>document.querySelector('.player');
+const video3 = document.querySelector<HTMLVideoElement>('.player');
+const video4 = document.querySelector('.player');
+
+video1.volume;
+video2.volume;
+video3?.volume;
+(video4 as HTMLVideoElement).volume;
