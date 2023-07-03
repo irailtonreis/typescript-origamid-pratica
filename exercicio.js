f9a96a4b36a497fa2dc3808476e44ca708e22004
irailtonreis <irailton.oliveira91@gmail.com>
2023-07-03 11:05:54 -0300

    feat: Exercício tuples

--- exercicio.js
+++ exercicio.js
@@ -43,3 +43,22 @@ function handleInput({ target }) {
 }
 const form = document.querySelector('#form');
 form?.addEventListener('keyup', handleInput);
+async function fetchVendas() {
+    const response = await fetch('https://api.origamid.dev/json/vendas.json');
+    const data = await response.json();
+    somarVendas(data);
+}
+function somarVendas(vendas) {
+    // Com for loop
+    let total1 = 0;
+    for (let i = 0; i < vendas.length; i++) {
+        total1 += vendas[i][1];
+    }
+    document.body.innerHTML += `<p>Total: R$ ${total1}</p>`;
+    // Com reduce
+    const total2 = vendas.reduce((total, venda) => {
+        return total + venda[1];
+    }, 0);
+    document.body.innerHTML += `<p>Total: R$ ${total2}</p>`;
+}
+fetchVendas();
